2847591385b5e10e5d5fc920fa2f4e5392ac109f
nelsonrsousa <nelson_85_sousa@hotmail.com>
2021-01-16 00:29:35 +0000

    updates prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: { 
+    apiKey: "AIzaSyCfTcU2AnPweLSQlJLHGRIpl1tTjUHxGCk",
+    authDomain: "vascobauerle-fe7a7.firebaseapp.com",
+    databaseURL: "https://vascobauerle-fe7a7.firebaseio.com",
+    projectId: "vascobauerle-fe7a7",
+    storageBucket: "vascobauerle-fe7a7.appspot.com",
+    messagingSenderId: "476425196211",
+    appId: "1:476425196211:web:d35b2a49868e2943d696c6"
+  } 
 };
