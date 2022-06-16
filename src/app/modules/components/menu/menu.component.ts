dc7d0dfb9fcc2c62be2786470f4e49971123a71e
nelsonrsousa <nelson_85_sousa@hotmail.com>
2022-06-16 16:40:12 +0100

    logica menu

--- src/app/modules/components/menu/menu.component.ts
+++ src/app/modules/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { WorksService } from 'src/app/services/works.service';
 
 @Component({
@@ -10,12 +11,26 @@ export class MenuComponent implements OnInit {
 
   tipos = [];
 
-  constructor(private worksService: WorksService) { }
+  showSecondMenu: boolean;
+
+  constructor(private worksService: WorksService,  private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
   
-    this.tipos = this.worksService.getTypes().filter(tipo => tipo != 'home' && tipo != 'works');
+    this.tipos = this.worksService.getTypes().filter(tipo => tipo != 'home' && tipo != 'work');
     this.tipos.unshift('all');
+
+      let array = window.location.href.split("/");
+
+      console.log(array[array.length-1]);
+
+      let currentRoute = array[array.length-1]
+
+
+      this.showSecondMenu = !(currentRoute == 'home' || currentRoute == 'about')
+
+  
+
   }
 
 }
